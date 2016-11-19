86abefeb7299f6a96a6db9e89cfabdda5d5d306a
Alexandra Ilianova <karolinanik@gmail.com>
2016-11-19 23:23:41 +0000

    First passed test

--- src/test/java/AITest1.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created on 11/19/2016.
- */
-
-public class AITest1 {
-}
---
+++ src/test/java/aiTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created on 11/19/2016.
+ */
+public class aiTest {
+
+    @Test
+    public void testChrome ()
+    {
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://google.co.uk");
+        driver.quit();
+    }
+}
